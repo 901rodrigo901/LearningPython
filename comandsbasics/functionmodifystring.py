8ebd1c4a202c642da583608e389cbe519a059d89
901rodrigo901 <rodrigo_piera@hotmail.com>
2020-01-11 12:05:12 -0800

    just basic commands

---
+++ comandsbasics/functionmodifystring.py
@@ -0,0 +1,9 @@
+sentence = 'the dog is named Sammy'
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.capitalize())
+print(sentence.count('a'))
+
+first_name = input('What is your first name?')
+last_name = input('What is your last name?')
+print('Hello ' + first_name.capitalize() + ' ' + last_name.capitalize())
